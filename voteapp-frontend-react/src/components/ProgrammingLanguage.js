9aff9e6455e0b28c551ab198c8019aaa701b9927
anasuya <anasuya.gudivada@zemosolabs.com>
2023-07-17 16:59:06 +0530

    changed url

--- voteapp-frontend-react/src/components/ProgrammingLanguage.js
+++ voteapp-frontend-react/src/components/ProgrammingLanguage.js
@@ -17,13 +17,13 @@ class ProgrammingLanguage extends Component {
 
   componentDidMount () {
     //provide implementation to request language details for current language from the API server
-    axios.get(`http://${this.APIHOSTPORT}/languages/${this.props.id}`).then(
+    axios.get(`https://${this.APIHOSTPORT}/languages/${this.props.id}`).then(
       response => this.setState({
         language: response.data,
         loaded: true
       })
     );
-    console.log(`http://${this.APIHOSTPORT}/languages/${this.props.id}`);
+    // console.log(`http://${this.APIHOSTPORT}/languages/${this.props.id}`);
   }
 
   render () {
